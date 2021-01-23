71176af1820d7d1786a1942034970212f3db29a0
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2021-01-23 18:19:51 +0800

    Format

--- Dino Game/script.js
+++ Dino Game/script.js
@@ -1,20 +1,20 @@
-document.getElementById("btn").onclick=function(){
-    document.getElementById("btn").style.display='none';
+document.getElementById("btn").onclick = function () {
+    document.getElementById("btn").style.display = "none";
 
-    const dino = document.getElementById('dino');
-    const dinoImg = dino.querySelector('img');
-    const cactus = document.getElementById('cactus');
-    const cactusImg = cactus.querySelector('img');
+    const dino = document.getElementById("dino");
+    const dinoImg = dino.querySelector("img");
+    const cactus = document.getElementById("cactus");
+    const cactusImg = cactus.querySelector("img");
     // const bird = document.getElementById('bird');
     // const birdImg = bird.querySelector('img');
-    
-    let Score = document.getElementsByTagName('i')[0];
-    let scoreNum=0;
+
+    let Score = document.getElementsByTagName("i")[0];
+    let scoreNum = 0;
     let dinoDownTF = false;
     let dinoJumpTF = false;
     let dinoCanLive = true;
     function jump() {
-        if (dino.classList != 'jump') {
+        if (dino.classList != "jump") {
             dino.classList.add("jump");
             setTimeout(() => {
                 dino.classList.remove("jump");
@@ -23,41 +23,46 @@ document.getElementById("btn").onclick=function(){
     }
     function down() {
         dinoDownTF = true;
-        dinoImg.setAttribute('src', 'img/dinoDown1.png')
-        
-        document.addEventListener('keyup',control1);
+        dinoImg.setAttribute("src", "img/dinoDown1.png");
+
+        document.addEventListener("keyup", control1);
         function control1() {
-            dinoDownTF=false;
-            dinoImg.setAttribute('src', 'img/dino.png')
+            dinoDownTF = false;
+            dinoImg.setAttribute("src", "img/dino.png");
         }
     }
     function control(e) {
-        if(e.keyCode===32 || e.keyCode===38){
+        if (e.keyCode === 32 || e.keyCode === 38) {
             // jump code
-    
+
             jump();
         } else if (e.keyCode === 40) {
             down();
         }
-    
     }
     // 成绩
-    let scoreTime= setInterval(() => {
+    let scoreTime = setInterval(() => {
         scoreNum++;
-        Score.innerHTML=scoreNum;
+        Score.innerHTML = scoreNum;
     }, 1000);
-    
+
     let dinoAlive = setInterval(() => {
         // 获得恐龙高度距离
-        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        let dinoTop = parseInt(
+            window.getComputedStyle(dino).getPropertyValue("top")
+        );
         // 获得仙人掌宽度距离
-        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+        let cactusLeft = parseInt(
+            window.getComputedStyle(cactus).getPropertyValue("left")
+        );
         // 获得鸟的宽度距离
-        let birdLeft = parseInt(window.getComputedStyle(bird).getPropertyValue("left"));
+        let birdLeft = parseInt(
+            window.getComputedStyle(bird).getPropertyValue("left")
+        );
         if (cactusLeft < 40 && cactusLeft > 0 && dinoTop >= 100) {
-            dinoImg.setAttribute('src', 'img/dinoDie.png');
-            alert('you die!');
-            window.location.reload(); 
+            dinoImg.setAttribute("src", "img/dinoDie.png");
+            alert("you die!");
+            window.location.reload();
         }
         if (dinoTop <= 76 || dinoDownTF == true) {
             dinoCanLive = true;
@@ -65,38 +70,37 @@ document.getElementById("btn").onclick=function(){
             dinoCanLive = false;
         }
         if (birdLeft < 40 && birdLeft > 0 && dinoCanLive == false) {
-            dinoImg.setAttribute('src', 'img/dinoDie.png');
-            alert('you die!');
-            window.location.reload(); 
+            dinoImg.setAttribute("src", "img/dinoDie.png");
+            alert("you die!");
+            window.location.reload();
         }
-        if(dinoTop<140){
-            dinoImg.setAttribute('src','img/dino.png');
+        if (dinoTop < 140) {
+            dinoImg.setAttribute("src", "img/dino.png");
         }
     }, 10);
-    document.addEventListener('keydown',control);
-    
+    document.addEventListener("keydown", control);
+
     // 更换仙人掌图片
     setInterval(() => {
-        let num=Math.floor(Math.random()*10); 
+        let num = Math.floor(Math.random() * 10);
         console.log(num);
-        if(num<=3){
-            cactusImg.setAttribute('src','img/cactus2.png')
-        }else if(3<num<=6){
-            cactusImg.setAttribute('src','img/cactus3.png')
-        }else if(6<num<=10){
-            cactusImg.setAttribute('src','img/cactus4.png')
+        if (num <= 3) {
+            cactusImg.setAttribute("src", "img/cactus2.png");
+        } else if (3 < num <= 6) {
+            cactusImg.setAttribute("src", "img/cactus3.png");
+        } else if (6 < num <= 10) {
+            cactusImg.setAttribute("src", "img/cactus4.png");
         }
     }, 2000);
     // 鸟和仙人掌出现
     setInterval(() => {
-        let ShowTime = Math.floor(Math.random()*10); 
-        if(ShowTime<5){
+        let ShowTime = Math.floor(Math.random() * 10);
+        if (ShowTime < 5) {
             cactus.classList.add("cactusShow");
             bird.classList.remove("birdShow");
-        }else if(ShowTime>=5){
+        } else if (ShowTime >= 5) {
             bird.classList.add("birdShow");
             cactus.classList.remove("cactusShow");
         }
     }, 2000);
-    }
-    
+};
--- Memory Game/script.js
+++ Memory Game/script.js
@@ -1,123 +1,123 @@
 const Btn = document.getElementById("btn");
-Btn.onclick=function go(){
+Btn.onclick = function go() {
 
-Btn.style.display='none';
-const imgArr=[
-    {
-        name:'1',
-        img:'img/1.png'
-    },
-    {
-        name:'2',
-        img:'img/2.png'
-    },
-    {
-        name:'3',
-        img:'img/3.png'
-    },
-    {
-        name:'4',
-        img:'img/4.png'
-    },
-    {
-        name:'5',
-        img:'img/5.png'
-    },
-    {
-        name:'6',
-        img:'img/6.png'
-    },
-    {
-        name:'1',
-        img:'img/1.png'
-    },
-    {
-        name:'2',
-        img:'img/2.png'
-    },
-    {
-        name:'3',
-        img:'img/3.png'
-    },
-    {
-        name:'4',
-        img:'img/4.png'
-    },
-    {
-        name:'5',
-        img:'img/5.png'
-    },
-    {
-        name:'6',
-        img:'img/6.png'
-    }
-]
+    Btn.style.display = 'none';
+    const imgArr = [
+        {
+            name: '1',
+            img: 'img/1.png'
+        },
+        {
+            name: '2',
+            img: 'img/2.png'
+        },
+        {
+            name: '3',
+            img: 'img/3.png'
+        },
+        {
+            name: '4',
+            img: 'img/4.png'
+        },
+        {
+            name: '5',
+            img: 'img/5.png'
+        },
+        {
+            name: '6',
+            img: 'img/6.png'
+        },
+        {
+            name: '1',
+            img: 'img/1.png'
+        },
+        {
+            name: '2',
+            img: 'img/2.png'
+        },
+        {
+            name: '3',
+            img: 'img/3.png'
+        },
+        {
+            name: '4',
+            img: 'img/4.png'
+        },
+        {
+            name: '5',
+            img: 'img/5.png'
+        },
+        {
+            name: '6',
+            img: 'img/6.png'
+        }
+    ]
     imgArr.sort(function () { return 0.5 - Math.random(); });
     const img = document.getElementsByTagName('img');
     const Memorybox = document.getElementById('Memorybox');
     let Score = document.getElementsByTagName('i')[0];
-    
-    let scoreNum=0;
+
+    let scoreNum = 0;
 
     let cardId = [];
     let cardArr = [];
     let imgIdArr = [];
     let addNum = 0;
-    Memorybox.className = 'Memorybox';    
+    Memorybox.className = 'Memorybox';
     function detect() {
         // var cards = document.querySelectorAll('img');
         const cardArrOne = imgIdArr[0];
         const cardArrTwo = imgIdArr[1];
         console.log(cardArrOne);
         console.log(cardArrTwo);
-        if(cardArrOne==cardArrTwo){
-            cardArr[0].setAttribute('src','img/close.png');
-            cardArr[1].setAttribute('src','img/close.png');
-        }else if(cardId[0]===cardId[1]){
-            addNum+=1;
-            cardArr[0].setAttribute('src','img/open.png');
-            cardArr[1].setAttribute('src','img/open.png');
-            cardArr[0].setAttribute('style','pointer-events:none')
-            cardArr[1].setAttribute('style','pointer-events:none')
-            if(addNum==6){
-                document.querySelector('h1').style.display='block';
-                Btn.innerHTML="重新开始"
-                Btn.style.display='block';
+        if (cardArrOne == cardArrTwo) {
+            cardArr[0].setAttribute('src', 'img/close.png');
+            cardArr[1].setAttribute('src', 'img/close.png');
+        } else if (cardId[0] === cardId[1]) {
+            addNum += 1;
+            cardArr[0].setAttribute('src', 'img/open.png');
+            cardArr[1].setAttribute('src', 'img/open.png');
+            cardArr[0].setAttribute('style', 'pointer-events:none')
+            cardArr[1].setAttribute('style', 'pointer-events:none')
+            if (addNum == 6) {
+                document.querySelector('h1').style.display = 'block';
+                Btn.innerHTML = "重新开始"
+                Btn.style.display = 'block';
                 clearInterval(scoreTime);
             }
             console.log(cardArr[0]);
             console.log(cardArr[1]);
-            }else{
-            cardArr[0].setAttribute('src','img/close.png');
-            cardArr[1].setAttribute('src','img/close.png');
-            
+        } else {
+            cardArr[0].setAttribute('src', 'img/close.png');
+            cardArr[1].setAttribute('src', 'img/close.png');
+
         }
         // 重置
-        cardArr=[]
-        cardId=[]
-        imgIdArr=[]
+        cardArr = []
+        cardId = []
+        imgIdArr = []
     }
     for (let i = 0; i < img.length; i++) {
         console.log(imgArr[i]);
-        img[i].addEventListener('click',flipCard);
-        function flipCard(){
+        img[i].addEventListener('click', flipCard);
+        function flipCard() {
 
-            img[i].setAttribute('src',imgArr[i].img)
-            img[i].index=i;
+            img[i].setAttribute('src', imgArr[i].img)
+            img[i].index = i;
             imgIdArr.push(this.index);
             cardArr.push(img[i]);
             cardId.push(imgArr[i].name);
-            if(cardId.length===2){
+            if (cardId.length === 2) {
                 setTimeout(detect, 300);
             }
         }
     }
     // 成绩
-    let scoreTime= setInterval(() => {
+    let scoreTime = setInterval(() => {
         scoreNum++;
-        Score.innerHTML=scoreNum;
+        Score.innerHTML = scoreNum;
     }, 1000);
     document.getElementById("btn").onclick = function () {
-        window.location.reload(); 
+        window.location.reload();
     }
 }
