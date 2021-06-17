a1f1a0f3e9d31a3c3a54b864331cd85fa4cdaf71
Khaki46 <39086187+Khaki46@users.noreply.github.com>
2021-06-17 23:16:50 +0800

    let ball move

--- Eatball Game/script.js
+++ Eatball Game/script.js
@@ -4,46 +4,88 @@ const canvas = document.querySelector('canvas');
 
 const ctx = canvas.getContext('2d');
 
-const Width = gameBox.offsetWidth;
-const Height = gameBox.offsetHeight;
+const Width = canvas.width = gameBox.offsetWidth;
+const Height = canvas.height = gameBox.offsetHeight;
 
-function random(min, max) {
+function random(min,max) {
     return Math.floor(Math.random()*(max-min)) + min;
-}
+  }
 function randomColor() {
     return 'rgb(' +
-    random(0, 255) + ', ' +
-    random(0, 255) + ', ' +
-    random(0, 255) + ')';
-}
+           random(0, 255) + ', ' +
+           random(0, 255) + ', ' +
+           random(0, 255) + ')';
+  }
 
-function Ball(x, y, color, size) {
+function Ball(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
+    this.velX = velX;
+    this.velY = velY;
     this.color = color;
     this.size = size;
 }
-Ball.prototype.draw = () => {
+Ball.prototype.draw = function() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
 }
+Ball.prototype.update = function() {
+    if ((this.x + this.size) >= Width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= Height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  Ball.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+      if (this !== balls[j]) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = randomColor();
+        }
+      }
+    }
+  }
 let balls = [];
 while (balls.length < 10) {
     let size = random(10, 25);
     let ball = new Ball(
         random(0 + size, Width - size),
         random(0 + size, Height - size),
+        random(-7, 7),
+        random(-7, 7),
         randomColor(),
         size
-    );
+      );
     balls.push(ball);
 }
-function loop() {  
+function loop() {
+    ctx.fillRect(0, 0, Width, Height);
+
     for (let index = 0; index < balls.length; index++) {
-        balls[index].draw();        
+        balls[index].draw();
+        balls[index].update();
+        balls[index].collisionDetect();
     }
     requestAnimationFrame(loop);
-}
+
+  }
 loop();
